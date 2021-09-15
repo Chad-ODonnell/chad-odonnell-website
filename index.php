59d4c71483f378894fac0093372bd0652693d533
Chad O'Donnell <76896489+Chad-ODonnell@users.noreply.github.com>
2021-09-15 14:22:22 -0500

    Commented out SQL

--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
 
   // Connect to the Database.
-  $connect = mysqli_connect('localhost', 'chad', 'MyNameIsBOB101', 'contact_information');
+  //$connect = mysqli_connect('localhost', 'chad', 'MyNameIsBOB101', 'contact_information');
 
   // Checks the connection.
-  if(!$connect){
-    echo 'Connection Error: ' . mysqli_connect_error();
-  }
+  //if(!$connect){
+    //echo 'Connection Error: ' . mysqli_connect_error();
+  //}
 
   // Write Query for Contact Info.
-  $sql = 'SELECT fname, lname, email, phone, id, inquiry FROM contactinfo';
+  //$sql = 'SELECT fname, lname, email, phone, id, inquiry FROM contactinfo';
 
   // Make query and get result.
-  $result = mysqli_query($connect, $sql);
+  //$result = mysqli_query($connect, $sql);
 
   // Fetch the resulting rows as an array.
-  $contact = mysqli_fetch_all($result, MYSQLI_ASSOC);
+  //$contact = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-  mysqli_free_result($result);
+  //mysqli_free_result($result);
 
   // Close connection via $connect.
-  mysqli_close($connect);
+  //mysqli_close($connect);
 
   //print_r($contact);
 
